b757d7499cd7dfa34ec0a598fa30d5c19eb881ad
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2020-12-23 20:18:07 -0800

    added skills section, design changes

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// fixed navbar
+// make navbar fixed
 window.addEventListener("scroll", () => {
   const nav = document.querySelector("nav");
   nav.classList.toggle("fixed", window.scrollY > 0);
+});
+
+// reveal projects when scrolling down
+window.addEventListener("scroll", () => {
+  const reveal = document.querySelector(".reveal");
+
+  const windowHeight = window.innerHeight;
+  const revealTop = reveal.getBoundingClientRect().top;
+  const revealPoint = 150;
+
+  if (revealTop < windowHeight - revealPoint) {
+    reveal.classList.add("active");
+  }
 });
 
 // hamburger-menu
